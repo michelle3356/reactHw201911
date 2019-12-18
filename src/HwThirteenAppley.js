fd016c626584b378292680ef204f864038a13afc
michelle3356 <michelle3356@yahoo.com.tw>
2019-12-19 01:30:21 +0800

    1219index bind

--- src/HwThirteenAppley.js
+++ src/HwThirteenAppley.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
-import './App.css';
+import './HwThirteenAppley.css';
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state={
       value: "",
-      toDoList: [],
       toDoListdata :[
         { id: Math.random(), content: '待辦事項1', done: false },
         { id: Math.random(), content: '待辦事項2', done: false },
         { id: Math.random(), content: '待辦事項3', done: true }
-      ],
-      isShowDone: false
+      ]
     };
   }
   
@@ -26,43 +24,51 @@ export default class App extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(e)
-    const toDoList = this.state.toDoList;
+    const toDoListdata = this.state.toDoListdata;
     const value = this.state.value;
     if(value !== '' && value.trim() !== '')
     this.setState({
-      toDoList: [value, ...toDoList],
+      toDoListdata: [{id: Math.random(), content: value, done: false}, ...toDoListdata],
       value: ''
     })
   };
   
-  handleClick = (e) => {
-    console.warn(e)
-    console.warn(e.target)
+  handleClick = (targetIndex) => {
+    console.warn(targetIndex)
+    var clickList = this.state.toDoListdata.map((item, index) =>
+        console.log(item, index)
+        // console.log(item.done)
+        // targetIndex === index  !(item.done)
+      )
     this.setState({
-      isShowDone: e.target
+      toDoListdata: clickList
     })
   };
   
   render() {
     // console.warn(this.state.toDoListdata);
-    const toDoList = this.state.toDoListdata.map((item, index) =>
-    //!!不要再把ul寫進來map裡面了!!
-    <li key={item.id} onClick={this.handleClick}>{item.content}</li>
+    const toDoListdata = this.state.toDoListdata.map((item, index) =>
+      //!!不要再把ul寫進來map裡面了!!
+      <li
+        key={item.id}
+        onClick={this.handleClick.bind(this, index)}
+        className={item.done ? 'done': ''}
+      >{item.content}</li>
     )
     
     return (
       <div>
-      <div>一開始你會有一個清單，內有3個項目，內容如下：</div>
-      <form onSubmit={this.handleSubmit}>
-      <input type="text"
-        onChange={this.handleChange}
-      />
-      <button>submit</button>
-      </form>
-      {/* !!不要再把ul寫進去map裡面了!! */}
-      <ul>
-      {toDoList}
-      </ul>
+        <div>一開始你會有一個清單，內有3個項目，內容如下：</div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+            onChange={this.handleChange}
+          />
+          <button>submit</button>
+        </form>
+        {/* !!不要再把ul寫進去map裡面了!! */}
+        <ul>
+          {toDoListdata}
+        </ul>
       </div>
       )
     }
