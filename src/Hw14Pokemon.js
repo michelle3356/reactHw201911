9e3c8f03f43b1ff5a088dee3197fcdd80f70a638
michelle3356 <michelle3356@yahoo.com.tw>
2019-12-27 11:26:35 +0800

    image 1 to 9 not yet

--- src/Hw14Pokemon.js
+++ src/Hw14Pokemon.js
@@ -16,22 +16,28 @@ export default class Hw14Pokemon extends Component {
     }
   }
 
-  handleChange = (e) =>{
-    this.setState({
-      keyword:e.target.value
-    })
-    this.callApi(this.state.pok)
-  }
+  /**
+   * 你這邊只是定義 callApi 這個程式，但沒有呼叫，如果你要呼叫的話，不用特別寫 this.callApi = XXX
+   * 直接呼叫就可以了，像這樣…
+   * 這樣明白嗎？
+   * 明白
+   * 然後呢，如果你的 => FUNCTION 只有一個return值，你可以省略 return 與 { .... }
+   * 那是當然的，因為 url 根本就不是圖片的網址，而且我的說明裡其實就有講到…↓
 
-  callApi = () =>{
+   */
+  componentDidMount(){
     fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`)
-    .then(res=>{
-      return res.json()
+    .then(res => res.json())
     .then(json => {
       this.setState({
         pok: json.results
       })
-    })
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword:e.target.value
     })
   }
   render() {
@@ -41,8 +47,13 @@ export default class Hw14Pokemon extends Component {
         <h3>前提</h3>
         <div>
           <div>請使用 https://pokeapi.co/api/v2/pokemon/?limit=100 來抓取100取寶可夢資訊</div>
+          {/* 這邊有提到 id 可以從 url 中取得，但url並不是圖片位置，你要想辦法取的那個 id 
+          好那我再試試看謝謝～～
+          
+          */}
           <div>每隻寶可夢的資料皆有 name, url 可從 url 取得該寶可夢的 id</div>
           <div>每隻寶可夢的圖片位址為： </div>
+          {/*  我這邊就有說到，看這邊 */}
           <div>https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/[id].png</div>
         </div>
         <h3>目標</h3>
@@ -59,10 +70,17 @@ export default class Hw14Pokemon extends Component {
         </ol>
         <table>
         <tbody>
+        {/* 而且…你這邊的 results.index 是怎麼回事啊，你拿回來的資料裡，每個物件中有 index ????????? 
+        沒有 只是先...
+        如果你要設key，我建議你可以直接用 name，因為 pokemon的名字不會重複
+        */}
         {this.state.pok.map(function(results){
             return(
-            <tr key={results.index}>
-              <td><img src={results.url} /></td>
+            <tr key={results.name}>
+              {/* 然後這邊的url不是圖片位置，請注意。 */}
+              {/* 你先寫吧我上班 
+              好謝謝～*/}
+              <td><img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+(results.url).substring(34,36)+'.png'} /></td>
               <td>{results.name}</td>
             </tr>
             )
