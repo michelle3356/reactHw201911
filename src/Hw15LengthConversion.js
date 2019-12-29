2144d8e82535a04ad6b50f5310b3392e324484bf
michelle3356 <michelle3356@yahoo.com.tw>
2019-12-29 23:54:56 +0800

    HW15 error

--- src/Hw15LengthConversion.js
+++ src/Hw15LengthConversion.js
@@ -1,63 +1,95 @@
 import React, { Component } from 'react'
 import Hw15LCSon from './Hw15LCSon';
 
+function toMM(lengthNum){
+  return lengthNum / 100;
+}
+
+function toCM(lengthNum){
+  return lengthNum / 100;
+}
+
+function toM(lengthNum){
+  return lengthNum / 100;
+}
+
+function toKM(lengthNum){
+  return lengthNum / 100;
+}
+
+function convertLength(lengthNum, convert){
+  const input = parseFloat(lengthNum);
+  if(Number.isNaN(input)){
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 export default class Hw15LengthConversion extends Component {
   constructor(props){
     super(props);
     this.state = {
       lengthNum: "",
+      length: 'm'
     }
   }
 
-  handleMMChange = (value) => {
-    console.warn(value);
-    // this.setState({
-    //   lengthNum: value.target.value
-    // })
+  handleMMChange = (lengthNum) => {
+    console.warn(lengthNum);
+    this.setState({
+      length: 'mm', lengthNum
+    })
   }
 
-  handleCMChange = (value) => {
-    console.warn(value);
-    // this.setState({
-    //   lengthNum: value.target.value
-    // })
+  handleCMChange = (lengthNum) => {
+    console.warn(lengthNum);
+    this.setState({
+      length: 'cm', lengthNum
+    })
   }
 
-  handleMChange = (value) => {
-    console.warn(value);
-    // this.setState({
-    //   lengthNum: value.target.value
-    // })
+  handleMChange = (lengthNum) => {
+    console.warn(lengthNum);
+    this.setState({
+      length: 'm', lengthNum
+    })
   }
 
-  handleKMChange = (value) => {
-    console.warn(value);
-    // this.setState({
-    //   lengthNum: value.target.value
-    // })
+  handleKMChange = (lengthNum) => {
+    console.warn(lengthNum);
+    this.setState({
+      length: 'km', lengthNum
+    })
   }
 
   render() {
     const lengthNum = this.state.lengthNum;
+    const length = this.state.length;
+    const mm = length = 'mm' ? convertLength(lengthNum, toMM) : lengthNum;
+    const cm = length = 'cm' ? convertLength(lengthNum, toCM) : lengthNum;
+    const m = length = 'm' ? convertLength(lengthNum, toM) : lengthNum;
+    const km = length = 'km' ? convertLength(lengthNum, toKM) : lengthNum;
     return (
       <div>
       <h1>長度轉換</h1>
       <Hw15LCSon
       length="mm"
       onLengthTrans = {this.handleMMChange}
-      lengthNum = {lengthNum} />
+      lengthNum = {mm} />
       <Hw15LCSon
       length="cm"
       onLengthTrans = {this.handleCMChange}
-      lengthNum = {lengthNum} />
+      lengthNum = {cm} />
       <Hw15LCSon
       length="m"
       onLengthTrans = {this.handleMChange}
-      lengthNum = {lengthNum} />
+      lengthNum = {m} />
       <Hw15LCSon
       length="km"
       onLengthTrans = {this.handleKMChange}
-      lengthNum = {lengthNum} />
+      lengthNum = {km} />
       </div>
     )
   }
