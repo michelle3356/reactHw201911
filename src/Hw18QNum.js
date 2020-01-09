466bfd7c5bd45f5ea31fdbf12d36677233a36466
Ben Xie <ben.xie@xuenn.com>
2020-01-09 10:53:26 +0800

    hw18 修正

--- src/Hw18QNum.js
+++ src/Hw18QNum.js
@@ -17,21 +17,15 @@ export default class Hw18 extends Component {
     const qNum = this.state.qNum;
     const fraction = this.state.fraction;
     return (
-      <div>
-      <Container>
-      <h1>{qNum}</h1>
-      <h2>分數：{fraction}</h2>
-      
-      <Card>
-      <h5>出現Q按Q</h5>
-      <p>上面的字母會不定期顯示為 Q, 顯示為 Q 時，鍵盤按下 Q 則得1分，並且立刻改回 - ，沒出現 Q 或打其他鍵，則扣分。</p>
-      <input type="text" placeholder="todo..." />
-      </Card>
-      
-      </Container>
-      
-      
+      <div className="container">
+        <div className="card text-center mx-auto mt-4" style={{width: '18rem'}}>
+          <h1>{qNum}</h1>
+          <h2>分數：{fraction}</h2>
+          <h5>出現Q按Q</h5>
+          <p>上面的字母會不定期顯示為 Q, 顯示為 Q 時，鍵盤按下 Q 則得1分，並且立刻改回 - ，沒出現 Q 或打其他鍵，則扣分。</p>
+          <input type="text" placeholder="todo..." />
+        </div>
       </div>
       )
+    }
   }
-}
