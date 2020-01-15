d3b551a11319859fd4e756e5c3db85b8c1f014c4
michelle3356 <michelle3356@yahoo.com.tw>
2020-01-16 00:51:03 +0800

    hw19 finish

--- src/Hw19GopherSon.js
+++ src/Hw19GopherSon.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react'
+import { gropherButton } from './Hw19Gopher.css'
+const sign = ['-', 'G'];
 
 export default class Hw19GopherSon extends Component {
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (e) => {
-    if(e.target.value === 'G'){
-      this.props.hitScore(1)
-    }else{
-      this.props.hitScore(-1)
+    this.state = {
+      num: sign[Math.floor(Math.random() * 2)]
     }
   }
 
+  componentDidMount(){
+    this.tick();
+  };
 
+  tick() {
+    this.random = setTimeout(
+      () => {
+        this.changNum();
+        this.tick();
+      },
+      Math.floor(Math.random() * 4000) + 2000
+    );
+  }
+
+  changNum(){
+    this.setState({
+      num: this.state.num === 'G'? '-': 'G'
+    });
+  };
+
+  handleClick = () => {
+    this.props.hitScore(this.state.num === 'G' ? 1: -1);
+    this.setState({
+      num: '-'
+    })
+  }
 
   render() {
-    const num = this.props.num
+    const { num } = this.state;
     return (
-      <button onClick={this.handleClick.bind(this)} value={num}>{num}
+      <button className="gropherButton" onClick={this.handleClick}>
+        {num}
       </button>
     )
   }
--- src/Hw19HitGopher.js
+++ src/Hw19HitGopher.js
@@ -1,31 +1,11 @@
-import React, { Component } from 'react'
-import Hw19GopherSon from './Hw19GopherSon'
+import React, { Component } from 'react';
+import Hw19GopherSon from './Hw19GopherSon';
 
 export default class Hw19 extends Component {
   constructor(props){
     super(props);
-    this.state = ({
-      num: 'G',
+    this.state = {
       score: 0
-    })
-  };
-
-  componentDidMount(){
-    this.random = setInterval(
-      () => this.changNum(),
-      Math.floor(Math.random() * 9000)
-    );
-  };
-
-  changNum(){
-    if(this.state.num === 'G'){
-      this.setState({
-        num: '-'
-      })
-    }else{
-      this.setState({
-        num: 'G'
-      })
     }
   };
 
@@ -36,23 +16,22 @@ export default class Hw19 extends Component {
   }
 
   render() {
-    const num = this.state.num;
-    const score = this.state.score;
+    const { score } = this.state;
     return (
       <div>
         <h1>打地鼠遊戲 Hit the gopher</h1>
         <h1>分數：{score}</h1>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
         <br></br>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
         <br></br>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
-        <Hw19GopherSon hitScore={this.hitScore} num={num}/>
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
+        <Hw19GopherSon hitScore={this.hitScore} />
         <div>點擊G得1分，點擊減號扣分</div>
       </div>
       )
