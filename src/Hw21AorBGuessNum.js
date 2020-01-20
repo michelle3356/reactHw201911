6fb677c395737d7bd008468dabcdc1298af36b44
michelle3356 <michelle3356@yahoo.com.tw>
2020-01-21 00:54:21 +0800

    hw21 initial

---
+++ src/Hw21AorBGuessNum.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Hw21AorBGuessNum extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
