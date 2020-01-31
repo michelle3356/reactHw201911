f075480d247bff38adc07a15ca25f61847e7ee5f
michelle3356 <michelle3356@yahoo.com.tw>
2020-01-31 16:28:08 +0800

    hw21 initial

--- src/Hw21AorBGuessNum.js
+++ src/Hw21AorBGuessNum.js
@@ -4,7 +4,16 @@ export default class Hw21AorBGuessNum extends Component {
   render() {
     return (
       <div>
-        
+        <h1>猜數字</h1>
+        <ol>
+          <li>一進到遊戲電腦隨機產生4位不重複數字</li>
+          <li>使用者送出答案，若不符合「不重複4位數字」則跳錯誤訊息</li>
+          <li>送出的答案跟正確答案比較，位置一樣則A，位置不同則B</li>
+          <li>會累積過去猜過的答案與結果</li>
+          <li>如果猜到 4A 則遊戲結束，並可以另開新局。</li>
+        </ol>
+        <input type="text" />
+        <button>Guess!</button>
       </div>
     )
   }
