3e33000a5b176fabad4917ac39ae870c465cf900
michelle3356 <michelle3356@yahoo.com.tw>
2020-02-10 11:51:50 +0800

    data bind

--- src/Hw22ShopCar.js
+++ src/Hw22ShopCar.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import Hw22ShopCarItem from './Hw22ShopCarItem'
 export default class Hw22ShopCar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Fruit: '',
+      price: ''
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-8">
             <div className="row">
-              <Hw22ShopCarItem />
+              <Hw22ShopCarItem Fruit="橘子" Price="20" />
+              <Hw22ShopCarItem Fruit="蓮霧" Price="30" />
+              <Hw22ShopCarItem Fruit="哈密瓜" Price="40" />
+              <Hw22ShopCarItem Fruit="蘋果" Price="50" />
+              <Hw22ShopCarItem Fruit="葡萄柚" Price="15" />
             </div>
           </div>
           <div className="col-sm-4">
-          42544
+            <ul>
+              
+            </ul>
           </div>
         </div>
       </div>
--- src/Hw22ShopCarItem.js
+++ src/Hw22ShopCarItem.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 import './Hw22.css'
 export default class Hw22ShopCarItem extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      num: '',
+      list: []
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const list = this.state.list;
+    list.push(this.props.Fruit + ' * ' + this.props.Price + ' = $' + this.state.num * this.props.Price)
+    console.log(list)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      num: e.target.value
+    })
+  }
   render() {
+    const Fruit = this.props.Fruit;
+    const Price = this.props.Price;
+    const num = this.state.num;
     return (
       <div className="col-sm-3">
         <div className="Fruit">
-          <div>one peace</div>
-          <div>$$</div>
-          <form>
-            <input type="number" placeholder="數量"></input>
-            <button className="btn btn-primary">加入購物車</button>
+          <div>{Fruit}</div>
+          <div>${Price}</div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="number" 
+            placeholder="數量" 
+            onChange = {this.handleChange}
+            value={num} />
+            <button className="btn btn-primary">Add to cart</button>
           </form>
         </div>
       </div>
