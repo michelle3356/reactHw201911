4a853eb9858ae2c62162b4fb77fefdb3e5f39ea1
michelle3356 <michelle3356@yahoo.com.tw>
2020-02-16 20:49:34 +0800

    hw23 initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homework from './Hw22ShopCar';
+import Homework from './Hw23SunMoon';
 
 function App() {
   return (
---
+++ src/Hw23SunMoon.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import './Hw23.css';
+
+export default class Hw23SunMoon extends Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+      oneDay: 'sun',
+    })
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      oneDay: 'sun' ? 'moon' : 'sun'
+    })
+  }
+  
+
+  render() {
+    const oneDay = this.state.oneDay;
+    return (
+      <div className={'HW23SunMoon text-center ' + oneDay}>
+        <div>
+          太陽圖片: https://img.icons8.com/cotton/256/000000/sun--v1.png
+          <br></br>
+          月亮圖片: https://img.icons8.com/cotton/256/000000/moon-satellite.png
+          <br></br>
+          請仔細觀察太陽與月亮的位置，「必須」對準正中央
+        </div>
+        <div className="dashboard">
+          <button className="btn btn-primary" onClick={this.handleClick.bind(this)}>切換成{oneDay === 'sun' ? '晚上' : '早上'}</button>
+        </div>
+        <div className="icon">
+          <div>
+            <img src="https://img.icons8.com/cotton/256/000000/sun--v1.png"></img>
+          </div>
+          <div>
+            <img src="https://img.icons8.com/cotton/256/000000/moon-satellite.png"></img>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
