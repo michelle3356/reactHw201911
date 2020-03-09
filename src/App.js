b062386578a575414b53a9d07c0718c93ae5697d
michelle3356 <michelle3356@yahoo.com.tw>
2020-03-09 23:53:13 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
-import Homework from './Hw24Lottery';
+import Home from './Home';
+import Hw1Timer from './Hw1Timer';
+import Hw2TimerList from './Hw2TimerList';
+// import Homework from './Hw24Lottery';
 
 function App() {
   return (
     <div className="App">
-      <Homework />
+      {/* <Homework /> */}
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Hw1Timer"><Hw1Timer /></Route>
+          <Route path="/Hw2TimerList"><Hw2TimerList /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
